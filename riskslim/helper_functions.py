237dafe586d08554b9920892054cf004300ff526
Berk Ustun <ustunb@mit.edu>
2017-02-20 13:26:21 -0500

    updated functions for loading data

--- riskslim/helper_functions.py
+++ riskslim/helper_functions.py
@@ -87,6 +87,24 @@ def get_or_set_default(settings, setting_name, default_value, type_check=False, 
 
 # LOADING DATA FROM DISK
 def load_data_from_csv(dataset_csv_file, sample_weights_csv_file = None):
+    """
+
+    Parameters
+    ----------
+    dataset_csv_file = location of a csv file on disk
+    sample_weights_csv_file = location of a sample weights file on disk
+
+    Returns
+    -------
+    dictionary containing training data for a binary classification problem with the fields:
+
+     - 'X' N x P matrix of features (numpy.ndarray) with a column of 1s for the '(Intercept)'
+     - 'Y' N x 1 vector of labels (+1/-1) (numpy.ndarray)
+     - 'variable_names' list of strings containing the names of each feature (list)
+     - 'Y_name' string containing the name of the output (optional)
+     - 'sample_weights' N x 1 vector of sample weights, must all be positive
+
+    """
 
     if os.path.isfile(dataset_csv_file):
         df = pd.read_csv(dataset_csv_file, sep=',')
@@ -106,11 +124,11 @@ def load_data_from_csv(dataset_csv_file, sample_weights_csv_file = None):
     # setup X and X_names
     X_col_idx = [j for j in range(raw_data.shape[1]) if j not in Y_col_idx]
     X = raw_data[:, X_col_idx]
-    X_names = [data_headers[j] for j in X_col_idx]
+    variable_names = [data_headers[j] for j in X_col_idx]
 
     # insert a column of ones to X for the intercept
     X = np.insert(arr=X, obj=0, values=np.ones(N), axis=1)
-    X_names.insert(0, '(Intercept)')
+    variable_names.insert(0, '(Intercept)')
 
     if sample_weights_csv_file is None:
         sample_weights = np.ones(N)
@@ -123,44 +141,91 @@ def load_data_from_csv(dataset_csv_file, sample_weights_csv_file = None):
 
     data = {
         'X': X,
-        'X_names': X_names,
         'Y': Y,
-        'Y_name': Y_name,
+        'variable_names': variable_names,
+        'outcome_name': Y_name,
         'sample_weights': sample_weights,
     }
 
-
+    assert check_data(data)
     return data
 
 # DATA CHECK
-def check_data(X, X_names, Y):
+def check_data(data):
+    """
+    makes sure that 'data' contains training data that is suitable for binary classification problems
+    throws AssertionError if
+
+    'data' is a dictionary that must contain:
+
+     - 'X' N x P matrix of features (numpy.ndarray) with a column of 1s for the '(Intercept)'
+     - 'Y' N x 1 vector of labels (+1/-1) (numpy.ndarray)
+     - 'variable_names' list of strings containing the names of each feature (list)
+
+     data can also contain:
+
+     - 'Y_name' string containing the name of the output (optional)
+     - 'sample_weights' N x 1 vector of sample weights, must all be positive
+
+    Returns
+    -------
+    True if data passes checks
+
+    """
     # type checks
-    assert type(X) is np.ndarray, "type(X) should be numpy.ndarray"
-    assert type(Y) is np.ndarray, "type(Y) should be numpy.ndarray"
-    assert type(X_names) is list, "X_names should be a list"
+    assert type(data) is dict, "data should be a dict"
+
+    assert 'X' in data, "data should contain X matrix"
+    assert type(data['X']) is np.ndarray, "type(X) should be numpy.ndarray"
+
+    assert 'Y' in data, "data should contain Y matrix"
+    assert type(data['Y']) is np.ndarray, "type(Y) should be numpy.ndarray"
+
+    assert 'variable_names' in data, "data should contain variable_names"
+    assert type(data['variable_names']) is list, "variable_names should be a list"
+
+    X = data['X']
+    Y = data['Y']
+    variable_names = data['variable_names']
+
+    if 'Y_name' in data.keys():
+        assert type(data['Y_name']) is str, "Y_name should be a str"
 
     # sizes and uniqueness
     N, P = X.shape
     assert N > 0, 'X matrix must have at least 1 row'
     assert P > 0, 'X matrix must have at least 1 column'
-    assert len(Y) == N, 'len(Y) should be same as # of rows in X'
-    assert len(list(set(X_names))) == len(X_names), 'X_names is not unique'
-    assert len(X_names) == P, 'len(X_names) should be same as # of cols in X'
+    assert len(Y) == N, 'dimension mismatch. Y must contain as many entries as X. Need len(Y) = N.'
+    assert len(list(set(data['variable_names']))) == len(data['variable_names']), 'variable_names is not unique'
+    assert len(data['variable_names']) == P, 'len(variable_names) should be same as # of cols in X'
 
-    # X_matrix values
-    if '(Intercept)' in X_names:
-        assert all(X[:, X_names.index('(Intercept)')] == 1.0), "'(Intercept)' column should only be composed of 1s"
-    else:
-        warnings.warn("there is no column named '(Intercept)' in X_names")
+    # feature matrix
     assert np.all(~np.isnan(X)), 'X has nan entries'
     assert np.all(~np.isinf(X)), 'X has inf entries'
 
-    # Y vector values
-    assert all((Y == 1) | (Y == -1)), 'Y[i] should = [-1,1] for all i'
-    if all(Y == 1):
-        warnings.warn("all Y_i == 1 for all i")
-    if all(Y == -1):
-        warnings.warn("all Y_i == -1 for all i")
+    # offset in feature matrix
+    if '(Intercept)' in variable_names:
+        assert all(X[:, variable_names.index('(Intercept)')] == 1.0), "'(Intercept)' column should only be composed of 1s"
+    else:
+        warnings.warn("there is no column named '(Intercept)' in variable_names")
 
-        # TODO (optional) collect warnings and return those?
+    # labels values
+    assert all((Y == 1) | (Y == -1)), 'Need Y[i] = [-1,1] for all i.'
+    if all(Y == 1):
+        warnings.warn('Y does not contain any positive examples. Need Y[i] = +1 for at least 1 i.')
+    if all(Y == -1):
+        warnings.warn('Y does not contain any negative examples. Need Y[i] = -1 for at least 1 i.')
+
+    if 'sample_weights' in data:
+        sample_weights = data['sample_weights']
+        type(sample_weights) is np.ndarray
+        assert len(sample_weights) == N, 'sample_weights should contain N elements'
+        assert all(sample_weights > 0), 'sample_weights[i] > 0 for all i '
+
+        # by default, we set sample_weights as an N x 1 array of ones. if not, then sample weights is non-trivial
+        if not all(sample_weights == 1):
+            if len(np.unique(sample_weights)) < 2:
+                warnings.warn('note: sample_weights only has <2 unique values')
+
+    return True
 
