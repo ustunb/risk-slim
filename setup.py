eb77c0027e2d8d40cd6821f09848546a7637264b
Berk Ustun <ustunb@mit.edu>
2017-02-20 17:04:18 -0500

    changed setup.py (still not working)

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@ from __future__ import print_function
 import sys
 from setuptools import setup, find_packages
 
+DISTNAME = 'risk-slim'
+DESCRIPTION = "optimized risk scores on large-scale datasets, " + \
+              "with CPLEX and Python"
+AUTHOR = 'Berk Ustun'
+AUTHOR_EMAIL = 'ustunb@mit.edu'
+URL = 'https://github.com/ustunb/risk-slim'
+#LICENSE = 'new BSD'
+DOWNLOAD_URL = 'https://github.com/ustunb/risk-slim'
+VERSION = '0.0.0'
+
+
 with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
-
 
 try:
     import numpy
@@ -18,11 +28,15 @@ except ImportError:
     print('scipy is required during installation')
     sys.exit(1)
 
-setup(name='sklearn-template',
-      version='0.0.3',
-      description='A template for scikit-learn compatible packages',
-      author='Vighnesh Birodkar',
-      packages=find_packages(),
+
+setup(name=DISTNAME,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      description=DESCRIPTION,
       install_requires=INSTALL_REQUIRES,
-      author_email='vighneshbirodkar@nyu.edu',
+      #license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      zip_safe=False,
       )
