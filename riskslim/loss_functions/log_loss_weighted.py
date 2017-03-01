74f6f8dc0cca823a43751a9678bc5d88b06564e0
Berk Ustun <ustunb@mit.edu>
2017-03-01 15:52:56 -0500

    added specs

--- riskslim/loss_functions/log_loss_weighted.py
+++ riskslim/loss_functions/log_loss_weighted.py
@@ -1,10 +1,23 @@
 import numpy as np
 
 def log_loss_value(Z, weights, total_weights, rho):
-    #compute the slope of the logistic loss function in a way that is numerically stable
-    #loss_value: (1 x 1) scalar = 1/n_rows * sum(log( 1 .+ exp(-Z*rho))
-    #see also: http://stackoverflow.com/questions/20085768/
-    #compute loss value
+    """
+    computes the value and slope of the logistic loss in a numerically stable way
+    supports sample non-negative weights for each example in the training data
+    see http://stackoverflow.com/questions/20085768/
+
+    Parameters
+    ----------
+    Z               numpy.array containing training data with shape = (n_rows, n_cols)
+    rho             numpy.array of coefficients with shape = (n_cols,)
+    total_weights   numpy.sum(total_weights) (only included to reduce computation)
+    weights         numpy.array of sample weights with shape (n_rows,)
+
+    Returns
+    -------
+    loss_value  scalar = 1/n_rows * sum(log( 1 .+ exp(-Z*rho))
+
+    """
     scores = Z.dot(rho)
     pos_idx = scores > 0
     loss_value = np.empty_like(scores)
@@ -14,10 +27,27 @@ def log_loss_value(Z, weights, total_weights, rho):
     return loss_value
 
 def log_loss_value_and_slope(Z, weights, total_weights, rho):
-    #compute the value and slope of the logistic loss function in a way that is numerically stable
-    #loss_value: (1 x 1) scalar = 1/n_rows * sum(log( 1 .+ exp(-Z*rho))
-    #loss_slope: (n_cols x 1) vector = 1/n_rows * sum(-Z*rho ./ (1+exp(-Z*rho))
-    #see also: http://stackoverflow.com/questions/20085768/
+    """
+    computes the value and slope of the logistic loss in a numerically stable way
+    supports sample non-negative weights for each example in the training data
+    this function should only be used when generating cuts in cutting-plane algorithms
+    (computing both the value and the slope at the same time is slightly cheaper)
+
+    see http://stackoverflow.com/questions/20085768/
+
+    Parameters
+    ----------
+    Z               numpy.array containing training data with shape = (n_rows, n_cols)
+    rho             numpy.array of coefficients with shape = (n_cols,)
+    total_weights   numpy.sum(total_weights) (only included to reduce computation)
+    weights         numpy.array of sample weights with shape (n_rows,)
+
+    Returns
+    -------
+    loss_value  scalar = 1/n_rows * sum(log( 1 .+ exp(-Z*rho))
+    loss_slope: (n_cols x 1) vector = 1/n_rows * sum(-Z*rho ./ (1+exp(-Z*rho))
+
+    """
     scores = Z.dot(rho)
     pos_idx = scores > 0
     exp_scores_pos = np.exp(-scores[pos_idx])
@@ -40,13 +70,32 @@ def log_loss_value_and_slope(Z, weights, total_weights, rho):
     return loss_value, loss_slope
 
 def log_loss_value_from_scores(weights, total_weights, scores):
-    #compute the value and slope of the logistic loss function in a way that is numerically stable
-    #loss_value: (1 x 1) scalar = 1/n_rows * sum(log( 1 .+ exp(-Z*rho))
-    #loss_slope: (n_cols x 1) vector = 1/n_rows * sum(-Z*rho ./ (1+exp(-Z*rho))
-    #see also: http://stackoverflow.com/questions/20085768/
+    """
+    computes the logistic loss value from a vector of scores in a numerically stable way
+    where scores = Z.dot(rho)
+
+    see also: http://stackoverflow.com/questions/20085768/
+
+    this function is used for heuristics (discrete_descent, sequential_rounding).
+    to save computation when running the heuristics, we store the scores and
+    call this function to compute the loss directly from the scores
+    this reduces the need to recompute the dot product.
+
+    Parameters
+    ----------
+    scores          numpy.array of scores = Z.dot(rho)
+    total_weights   numpy.sum(total_weights) (only included to reduce computation)
+    weights         numpy.array of sample weights with shape (n_rows,)
+
+    Returns
+    -------
+    loss_value  scalar = 1/n_rows * sum(log( 1 .+ exp(-Z*rho))
+
+    """
     pos_idx = scores > 0
     loss_value = np.empty_like(scores)
     loss_value[pos_idx] = np.log1p(np.exp(-scores[pos_idx]))
     loss_value[~pos_idx] = -scores[~pos_idx] + np.log1p(np.exp(scores[~pos_idx]))
     loss_value = loss_value.dot(weights) / total_weights
+
     return loss_value
