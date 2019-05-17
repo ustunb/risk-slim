fixed minor issue with print_model where it was showing the wrong function to ccompute probabilities in cases where there was a negative sign

riskslim/helper_functions.py
@@ -366,9 +366,9 @@ def print_model(rho, data,  show_omitted_variables = False):
         intercept_val = 0
 
     if 'outcome_name' in data:
-        predict_string = "Pr(Y = +1) = 1/(1 + exp(%d - score))" % intercept_val
+        predict_string = "Pr(Y = +1) = 1.0/(1.0 + exp(-(%d + score))" % intercept_val
     else:
-        predict_string = "Pr(%s = +1) = 1/(1 + exp(%d - score)" % (data['outcome_name'].upper(), intercept_val)
+        predict_string = "Pr(%s = +1) = 1.0/(1.0 + exp(-(%d + score))" % (data['outcome_name'].upper(), intercept_val)
 
     if not show_omitted_variables:
         selected_ind = np.flatnonzero(rho_values)