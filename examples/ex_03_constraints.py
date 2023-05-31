8fa847ae33ea4e6d4158c8e895e749e1102bd7b5
ryanhammonds <rphammonds@ucsd.edu>
2023-05-30 17:22:59 -0700

    constraint doc example

--- examples/ex_03_constraints.py
+++ examples/ex_03_constraints.py
@@ -1,117 +1,127 @@
 """
-Contraints
-==========
+03. Constraints
+===============
 
+Adding constraints to the MIP.
 """
-# import os
-# import numpy as np
-# import cplex as cplex
-# import pprint
-# import riskslim
 
-# # data
-# import riskslim.coefficient_set
+###################################################################################################
 
-# data_name = "breastcancer"                                  # name of the data
-# data_dir = os.getcwd() + '/examples/data/'                  # directory where datasets are stored
-# data_csv_file = data_dir + data_name + '_data.csv'          # csv file for the dataset
-# sample_weights_csv_file = None                              # csv file of sample weights for the dataset (optional)
-
-# # problem parameters
-# max_coefficient = 5                                         # value of largest/smallest coefficient
-# max_L0_value = 5                                            # maximum model size
-# max_offset = 50                                             # maximum value of offset parameter (optional)
-# c0_value = 1e-6                                             # L0-penalty parameter such that c0_value > 0; larger values -> sparser models; we set to a small value (1e-6) so that we get a model with max_L0_value terms
-# w_pos = 1.00                                                # relative weight on examples with y = +1; w_neg = 1.00 (optional)
-
-# # load data from disk
-# data = riskslim.load_data_from_csv(dataset_csv_file = data_csv_file, sample_weights_csv_file = sample_weights_csv_file)
-# N, P = data['X'].shape
-
-# # create coefficient set and set the value of the offset parameter
-# coef_set = riskslim.CoefficientSet(variable_names=data['variable_names'], lb=-max_coefficient, ub=max_coefficient, sign=0)
-# coef_set.update_intercept_bounds(X = data['X'], y = data['Y'], max_offset = max_offset)
-
-# # create constraint
-# trivial_L0_max = P - np.sum(coef_set.C_0j == 0)
-# max_L0_value = min(max_L0_value, trivial_L0_max)
-
-# constraints = {
-#     'L0_min': 0,
-#     'L0_max': max_L0_value,
-#     'coef_set':coef_set,
-# }
+from pathlib import Path
+import numpy as np
+from sklearn import clone
+from riskslim import RiskSLIMClassifier, load_data_from_csv
 
 
-# # major settings (see riskslim_ex_02_complete for full set of options)
-# settings = {
-#     # Problem Parameters
-#     'c0_value': c0_value,
-#     'w_pos': w_pos,
-#     #
-#     # LCPA Settings
-#     'max_runtime': 300.0,                               # max runtime for LCPA
-#     'max_tolerance': np.finfo('float').eps,             # tolerance to stop LCPA (set to 0 to return provably optimal solution)
-#     'display_cplex_progress': True,                     # print CPLEX progress on screen
-#     'loss_computation': 'normal',                       # how to compute the loss function ('normal','fast','lookup')
-#     #
-#     # RiskSLIM MIP settings
-#     'drop_variables': False,
-#     #
-#     # LCPA Improvements
-#     'round_flag': False,                                # round continuous solutions with SeqRd
-#     'polish_flag': False,                               # polish integer feasible solutions with DCD
-#     'chained_updates_flag': False,                      # use chained updates
-#     'initialization_flag': False,                       # use initialization procedure
-#     'init_max_runtime': 300.0,                          # max time to run CPA in initialization procedure
-#     'add_cuts_at_heuristic_solutions': True,            # add cuts at integer feasible solutions found using polishing/rounding
-#     #
-#     # CPLEX Solver Parameters
-#     'cplex_randomseed': 0,                              # random seed
-#     'cplex_mipemphasis': 0,                             # cplex MIP strategy
-# }
+###################################################################################################
+# Load Data
+# ---------
+#
+# The example dataset in this tutorial is used to predict if a breast cancer tumor is beign or
+# malignant.
+#
 
-# # turn on at your own risk
-# settings['round_flag'] = False
-# settings['polish_flag'] = False
-# settings['chained_updates_flag'] = False
-# settings['initialization_flag'] = False
+# Load Data
+data_name = "breastcancer"
+data = load_data_from_csv(dataset_csv_file = Path(f'data/{data_name}_data.csv'))
+
+# Unpack data
+X = data["X"]
+y = data["y"]
+variable_names = data["variable_names"]
+outcome_name = data["outcome_name"]
+
+# Procedures and improvement settings
+settings = {}
+settings['drop_variables'] = False
+settings['initialization_flag'] = True
+settings['round_flag'] = False
+settings['polish_flag'] = True
+settings['chained_updates_flag'] = False
 
 
-# # initialize MIP for lattice CPA
-# mip_objects = riskslim.setup_lattice_cpa(data, constraints, settings)
+###################################################################################################
+# Base Estimator
+# --------------
+#
+# A base estimator is defined below and used to generate solutions with and without constraints.
+#
 
-# # add operational constraints
-# mip, indices = mip_objects['mip'], mip_objects['indices']
-# get_alpha_name = lambda var_name: 'alpha_' + str(data['variable_names'].index(var_name))
-# get_alpha_ind = lambda var_names: [get_alpha_name(v) for v in var_names]
+rs_base = RiskSLIMClassifier(
+    max_size=5,
+    max_coef=5,
+    max_abs_offset=50,
+    variable_names=data["variable_names"],
+    verbose=False,
+    settings=settings
+)
 
-# # to add a constraint like "either "CellSize" or "CellShape"
-# # you must formulate the constraint in terms of the alpha variables
-# # alpha[cell_size] + alpha[cell_shape] <= 1 to MIP
-# mip.linear_constraints.add(
-#         names = ["EitherOr_CellSize_or_CellShape"],
-#         lin_expr = [cplex.SparsePair(ind = get_alpha_ind(['UniformityOfCellSize', 'UniformityOfCellShape']),
-#                                      val = [1.0, 1.0])],
-#         senses = "L",
-#         rhs = [1.0])
+# Fit
+rs = clone(rs_base)
+rs.fit(X, y)
 
-# mip_objects['mip'] = mip
-
-# # pass MIP back to lattice CPA so that it will solve
-# model_info, mip_info, lcpa_info = riskslim.finish_lattice_cpa(data, constraints, mip_objects, settings)
-
-# #model info contains key results
-# pprint.pprint(model_info)
-# riskslim.print_model(model_info['solution'], data)
-
-# # mip_output contains information to access the MIP
-# mip_info['risk_slim_mip'] #CPLEX mip
-# mip_info['risk_slim_idx'] #indices of the relevant constraints
-
-# # lcpa_output contains detailed information about LCPA
-# pprint.pprint(lcpa_info)
+rs.scores
 
 
+###################################################################################################
+# Constraints
+# -----------
+#
+# Next, an estimator with constraints is created using the ``.add_constraint`` method. The added
+# constraint ensure that either ClumpThickness or MarginalAdhesion will be zero. This is done by
+# setting the var_type parameter to "alpha", which must be either zero or one for each parameter
+# in the constraint. Constraints should be placed on either "alpha" or "rho", obeying:
+#
+# -rho_min_i * alpha_i < coefficient_i < rho_max_i * alpha_i
+#
 
+rs_constrained = clone(rs_base)
 
+rs_constrained.add_constraint(
+    # Variable names
+    ['ClumpThickness', 'MarginalAdhesion'],
+    # Variable type ("rho" or "alpha")
+    "alpha",
+    # Constraint coefficients
+    [1., 1.],
+    # Right hand side
+    1.,
+    # Sense or (in)equality
+    "L",
+    # Name of constraint
+    "either_or"
+)
+
+rs_constrained.fit(X, y)
+
+rs_constrained.scores
+
+###################################################################################################
+#
+# Coefficients (rho) values may instead be used to directly constrain problem. The coefficient of
+# the ClumpThickness variable is constrained below to be a minimum of 2.
+#
+
+rs_constrained.add_constraint(
+    # Variable names
+    ['ClumpThickness'],
+    # Variable type ("rho" or "alpha")
+    "rho",
+    # Constraint coefficients
+    [1.],
+    # Right hand side
+    2.,
+    # Sense or (in)equality
+    "G",
+    # Name of constraint
+    "min_rho_thickness"
+)
+
+rs_constrained.fit(X, y)
+
+rs_constrained.scores
+
+# sphinx_gallery_start_ignore
+from plotly.io import show
+show(rs_constrained.create_report(only_table=True, show=False))
+# sphinx_gallery_end_ignore
